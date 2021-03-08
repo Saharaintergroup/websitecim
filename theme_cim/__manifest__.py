c7efe4a65f55eb41268b084b23bb8fbd15ef7c56
Ghaith Gammar <info@saharaintergroup.com>
2021-03-08 11:06:16 +0200

    Add files via upload

---
+++ theme_cim/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- encoding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Cim Theme',
+    'description': 'Cim website theme',
+    'category': 'Theme',
+    'sequence': 1,
+    'version': '14.0',
+    'depends': ['base', 'website'],
+    'data': [
+        "templates/assets.xml",
+        "templates/header.xml",
+        "templates/footer.xml",
+        "templates/home.xml",
+        "templates/aboutus.xml",
+        "templates/electronic_services.xml",
+        "templates/indications.xml",
+        "templates/advertisements.xml",
+        "templates/media_center.xml",
+        "templates/common_questions.xml",
+        "templates/ratings.xml",
+        "templates/menu.xml",
+    ],
+    'images': [
+        'static/description/cover.png',
+        'static/description/theme_cim_screenshot.png',
+    ],
+    'application': True,
+    'auto_install': False,
+}
---
+++ theme_cim/static/src/js/home.js
@@ -0,0 +1,120 @@
+odoo.define('theme_cim.home', function (require) {
+'use strict';
+
+var ajax = require('web.ajax');
+var core = require('web.core');
+
+var qweb = core.qweb;
+
+
+
+new WOW().init();
+
+
+    jQuery(document).ready(function ($) {
+        $('.my-news-ticker').AcmeTicker({
+            type:'marquee',/*horizontal/horizontal/Marquee/type*/
+            direction: 'left',/*up/down/left/right*/
+            speed: 0.05,/*true/false/number*/ /*For vertical/horizontal 600*//*For marquee 0.05*//*For typewriter 50*/
+            controls: {
+                toggle: $('.acme-news-ticker-pause'),/*Can be used for horizontal/horizontal/typewriter*//*not work for marquee*/
+            }
+        });
+
+        function animateElements() {
+              // skill circle progress bar
+                var skillLevel1 = $('.skill-item .first').data('skill-level');
+                var skillLevel2 = $('.skill-item .second').data('skill-level');
+                var skillLevel3 = $('.skill-item .third').data('skill-level');
+                var skillLevel4 = $('.skill-item .fourth').data('skill-level');
+                 console.log('skillLevel1===>',skillLevel1);
+                //first.circle
+                $('.first.circle').circleProgress({
+                value: skillLevel1/100,
+                size: 100,
+                thickness:10,
+                emptyFill: "#B2BDC8",
+                fill: {
+                    color: "#F9B40E"
+                }
+                }).on('circle-animation-progress', function(event, progress) {
+                $(this).find('h6').html(Math.round(skillLevel1 * progress) + '<span>%</span>');
+                });
+
+                //second.circle
+                $('.second.circle').circleProgress({
+                value: skillLevel2/100,
+                size: 100,
+                thickness:10,
+                emptyFill: "#B2BDC8",
+                fill: {
+                    color: "#F10020"
+                }
+                }).on('circle-animation-progress', function(event, progress) {
+                $(this).find('h6').html(Math.round(skillLevel2 * progress) + '<i>%</i>');
+                });
+
+                //third.circle
+                $('.third.circle').circleProgress({
+                value: skillLevel3/100,
+                size: 100,
+                thickness:10,
+                emptyFill: "#B2BDC8",
+                fill: {
+                    color: "#12BFFF"
+                }
+                }).on('circle-animation-progress', function(event, progress) {
+                $(this).find('h6').html(Math.round(skillLevel3 * progress) + '<span>%</span>');
+                });
+
+                //fourth.circle
+                $('.fourth.circle').circleProgress({
+                value: skillLevel4/100,
+                size: 100,
+                thickness:10,
+                emptyFill: "#B2BDC8",
+                fill: {
+                    color: "#C683FF"
+                }
+                }).on('circle-animation-progress', function(event, progress) {
+                $(this).find('h6').html(Math.round(skillLevel4 * progress) + '<span>%</span>');
+                });
+        }
+
+        // Show animated elements
+        animateElements();
+        $(window).scroll(animateElements);
+
+
+        $('.carousel-partners').owlCarousel({
+            loop:true,
+            nav:false,
+            dots:false,
+            margin:10,
+            navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+            autoplay:4000,
+            smartSpeed:2000,
+            responsive:{
+                0:{
+                    items:3,
+                },
+                576:{
+                    items:5,
+                },
+                768:{
+                    items:7,
+                },
+                992:{
+                    items:9,
+                },
+                1200:{
+                    items:10,
+                }
+            }
+        })
+
+
+
+
+    })
+});
