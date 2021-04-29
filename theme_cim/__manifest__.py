072ae696b6a05d84810a52a00e5a4d50e8100f7d
derbal-sah <DerB123pwd>
2021-04-29 23:01:44 +0100

    developement page List of bids

--- theme_cim/__manifest__.py
+++ theme_cim/__manifest__.py
@@ -6,7 +6,7 @@
     'description': 'Cim website theme',
     'category': 'Theme',
     'sequence': 1,
-    'version': '1.0',
+    'version': '1.1',
     'depends': ['base', 'website', 'website_form'],
     'data': [
         "security/ir.model.access.csv",
