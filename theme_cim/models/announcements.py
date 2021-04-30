1a494dcffc9ee8d8a1b45b665e8ac9df96f14233
derbal-sah <DerB123pwd>
2021-04-30 01:48:11 +0100

    developement page List of bids and details List of bids

--- theme_cim/models/announcements.py
+++ theme_cim/models/announcements.py
@@ -10,6 +10,7 @@ class PublicAnnouncements(models.Model):
     description = fields.Text(string='Description', required=True, translate=True)
     image = fields.Image("Photo")
 
+
 class AvailableJobs(models.Model):
     _name = "available.jobs"
     _description = "Available jobs"
@@ -18,10 +19,27 @@ class AvailableJobs(models.Model):
     description = fields.Text(string='Description', required=True, translate=True)
     image = fields.Image("Photo")
 
+
 class Bids(models.Model):
     _name = "bids"
     _description = "Bids"
 
+    def _default_website(self):
+        """ Find the first company's website, if there is one. """
+        company_id = self.env.company.id
+
+        if self._context.get('default_company_id'):
+            company_id = self._context.get('default_company_id')
+
+        domain = [('company_id', '=', company_id)]
+        return self.env['website'].search(domain, limit=1)
+
+    website_id = fields.Many2one('website', string="Website", ondelete='restrict', default=_default_website, domain="[('company_id', '=?', company_id)]")
+    company_id = fields.Many2one('res.company', 'Company')
     name = fields.Char(string="Title", required=True, translate=True)
     description = fields.Text(string='Description', required=True, translate=True)
+    reference = fields.Char(string='Reference Number')
+    bid_date = fields.Date(string='Bid Date')
+    expiration_date= fields.Date(string='Expiration Date')
     image = fields.Image("Photo")
+    attachment_id = fields.Many2one('ir.attachment', string='Bid request brochure', ondelete='cascade')
