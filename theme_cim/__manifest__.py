78301ff999c2d669e9f75d7ea8f36b3a1f0021ba
derbal-sah <DerB123pwd>
2021-05-17 01:10:35 +0100

    Vesrsion

--- theme_cim/__manifest__.py
+++ theme_cim/__manifest__.py
@@ -6,7 +6,7 @@
     'description': 'Cim website theme',
     'category': 'Theme',
     'sequence': 1,
-    'version': '1.7.7',
+    'version': '1.7.8',
     'depends': ['base', 'website', 'website_form'],
     'data': [
         # security
