babf2dc030141a778461256ae5d57c57ab2104ca
derbal-sah <DerB123pwd>
2021-05-18 00:26:44 +0100

    الطاقم التنفيذي

--- theme_cim/__manifest__.py
+++ theme_cim/__manifest__.py
@@ -6,7 +6,7 @@
     'description': 'Cim website theme',
     'category': 'Theme',
     'sequence': 1,
-    'version': '1.7.9',
+    'version': '1.8.0',
     'depends': ['base', 'website', 'website_form'],
     'data': [
         # security
