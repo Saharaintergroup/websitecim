f4c291a446d12253ff5941612cbcd0e8361febab
Your Name <you@example.com>
2021-06-10 18:19:55 +0200

    [MOD]time_edit

--- theme_cim/controllers/website.py
+++ theme_cim/controllers/website.py
@@ -8,7 +8,7 @@ class Website(Website):
     def index(self, **kw):
         """function home page"""
         super(Website, self).index(**kw)
-        data = {'current_time':(datetime.now() + timedelta(hours=3)).strftime('%H:%M'),'slider_home': request.env['news'].sudo().search([('in_slider','=',True)]),
+        data = {'current_time':(datetime.now() + timedelta(hours=2)).strftime('%H:%M'),'slider_home': request.env['news'].sudo().search([('in_slider','=',True)]),
                 'ticker_news': request.env['news'].sudo().search([]),
                 'company_partners': request.env['company.partners'].sudo().search([])}
         return http.request.render('theme_cim.new_homepage', data)
